1e01e6242e315c97438ce8dd56bf3b4dece1ecd1
belee git <belen.abigail.gp@gmail.com>
2024-01-04 23:11:53 -0300

    cambio bar

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.header('Análisis de los anuncios de venta de coches')
+st.header('Análisis de venta de coches')
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
@@ -26,18 +26,18 @@ if scat_button: # al hacer clic en el botón
     st.write('Gráfico de dispersión según el tipo de combustible')
             
     # crear un gráfico de dispersión
-    fig = px.scatter(car_data, x="model", y="count", color="fuel", symbol="fuel")
+    fig = px.scatter(car_data, x="model", y="model_year")
         
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
 if bar_button: # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Precios según modelos de coche')
+    st.write('Cantidad de modelo de coches según transmisión y combustible')
             
 
     # crear un gráfico de barra
-    fig = px.bar(car_data, x="model", y=["type", "transmission", "fuel"])
+    fig = px.bar(car_data, x="model", y=["transmission", "fuel"])
         
 
     # mostrar un gráfico Plotly interactivo
